db6f1cc5a51ee8997866d414efc9325428e7c67d
Shloimy <shloimyelbaum@gmail.com>
2024-06-02 21:02:41 +0300

    added some styling

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,6 @@ import HeaderTemp from './components/HeaderTemp.vue'
     <HeaderTemp />
   </header>
   <div>
-    <a href="" target="_self" class="test w-min">
-      <img src="./assets/logo-y.png" class="rounded-full h-48 p-2" alt="Glicks logo" />
-    </a>
   </div>
   <HelloWorld msg="Welcome to our deluxe menu!" />
 </template>
--- src/components/HeaderTemp.vue
+++ src/components/HeaderTemp.vue
@@ -11,5 +11,8 @@
             <li><a href="#">Contact</a></li>
         </ul>
     </nav>
+    <a href="" target="_self" class="test w-min">
+      <img src="../assets/logo-y.png" class="rounded-full h-48 p-2" alt="Glicks logo" />
+    </a>
     <h1 class="text-amber-700">Glicks Heimish Bakery</h1>
 </template>
