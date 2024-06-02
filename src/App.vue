c2e0510b95d45c7cb8d79ab684b58ee8deb97497
Shloimy <shloimyelbaum@gmail.com>
2024-06-02 20:28:18 +0300

    new glicks repo

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import HeaderTemp from './components/HeaderTemp.vue'
+</script>
+
+<template>
+  <header class="test bg-pink-100">
+    <HeaderTemp />
+  </header>
+  <div>
+    <a href="" target="_self" class="test w-min">
+      <img src="./assets/logo-y.png" class="rounded-full h-48 p-2" alt="Glicks logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Welcome to our deluxe menu!" />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/HeaderTemp.vue
@@ -0,0 +1,15 @@
+<script>
+    import { ref } from 'vue'
+</script>
+
+<template>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Menu</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+    <h1 class="text-amber-700">Glicks Heimish Bakery</h1>
+</template>
