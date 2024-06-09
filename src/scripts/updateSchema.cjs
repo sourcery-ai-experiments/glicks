96d4636a52fd04076664d6ab8fdf3660e7f46d20
Shloimy <shloimyelbaum@gmail.com>
2024-06-09 17:33:29 +0300

    added schema update succesfully will run before build

---
+++ src/scripts/updateSchema.cjs
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+// Function to update the schema with categories
+function updateSchema() {
+    const categoriesPath = path.join(__dirname, '../assets/data/categories.json');
+    const schemaPath = path.join(__dirname, '../schemas/itemsSchema.json');
+
+    // Read categories
+    const categoriesData = JSON.parse(fs.readFileSync(categoriesPath, 'utf8'));
+    const validCategories = categoriesData.categories.map(cat => cat.name);
+
+    // Read and update schema
+    const schemaData = JSON.parse(fs.readFileSync(schemaPath, 'utf8'));
+    schemaData.properties.items.items.properties.description.properties.categories.enum = validCategories;
+
+    // Write the updated schema back to file
+    fs.writeFileSync(schemaPath, JSON.stringify(schemaData, null, 2), 'utf8');
+    console.log('Schema updated with categories.');
+}
+
+// Run the function
+updateSchema();
