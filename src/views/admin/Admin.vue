3c9cfdd2263f17e85fdb8eaa31ce069241df9731
Shloimy <shloimyelbaum@gmail.com>
2024-06-14 14:01:31 +0300

    Trying to add back admin parts to git while keeping it a away from netlify deploys.

---
+++ src/views/admin/Admin.vue
@@ -0,0 +1,11 @@
+<template>
+  <Header />
+  <AdminHome />
+  <Footer />
+</template>
+
+<script setup>
+import Header from '../../components/Header.vue';
+import AdminHome from './AdminHome.vue';
+import Footer from '../../components/Footer.vue';
+</script>
