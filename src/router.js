7c83074f8d8aaadcf51a4a64784ba05ea5448428
Shloimy <shloimyelbaum@gmail.com>
2024-06-14 10:13:25 +0300

    added condition to import

--- src/router.js
+++ src/router.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Admin from './views/Admin.vue';
 import Home from './views/Home.vue'; // Assuming you have a Home component
 import NotFound from './views/NotFound.vue';
 
