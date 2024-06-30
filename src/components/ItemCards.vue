efb69770ebe71ca81e254702fc77e5cf0c705b33
Shloimy <shloimyelbaum@gmail.com>
2024-06-30 19:34:20 +0300

    rounded image in item card issue fix

--- src/components/ItemCards.vue
+++ src/components/ItemCards.vue
@@ -1,8 +1,8 @@
 <template>
     <div v-for="item in items" :key="item.id"
         class="group relative bg-white w-5/6 aspect-1 rounded-lg shadow-md pb-3 transition hover:scale-105 hover:cursor-pointer duration-300">
-        <div class="aspect-[4/3] w-full rounded-md group-hover:opacity-75 duration-300">
-            <img :src="'/images/' + item.imageSrc" :alt="item.imageAlt" class="w-full h-full object-cover" />
+        <div class="aspect-[4/3] w-full rounded-lg group-hover:opacity-75 duration-300">
+            <img :src="'/images/' + item.imageSrc" :alt="item.imageAlt" class="w-full h-full object-cover rounded-lg" />
         </div>
         <h3 class="mt-4 text-lg">
             <a @click="openModal(item)" type="button" class="text-amber-700">
